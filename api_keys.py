a5b4d09501cb1f1d1bcdedd8f6514244a05f4952
alisonlitton <63117075+alisonlitton@users.noreply.github.com>
2020-07-12 21:03:07 -0700

    Update api_keys.py
    ADDED API KEY CODES

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "8313340f806965ca9ebe57a4ea4f6435"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBrmbjSfDbUW2QjMs7HQ6VA4TBBSUNn1ws"
